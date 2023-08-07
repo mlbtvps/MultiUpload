a91a83a7fe078c6ac2fbd9727e432dcc2ca6ff6d
mlbtvps <119069067+mlbtvps@users.noreply.github.com>
2023-08-08 03:32:55 +0630

    Update forPyrogram.py

--- TokenGeneration/forPyrogram.py
+++ TokenGeneration/forPyrogram.py
@@ -3,8 +3,8 @@ from pyrogram import Client
 
 
 #get your api_id and api_hash from https://my.telegram.org/apps
-api_id = 12345  # your api_id
-api_hash = "0123456789abcdef0123456789abcdef" # your api_hash
+api_id = 27486644  # your api_id
+api_hash = "149f309eb1c416c8edac85bba6c5eec2" # your api_hash
 
 
 async def main():
